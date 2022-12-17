f4b6128cebbd001807bae807ccf887980556d399
Oleg <olegashepelev@gmail.com>
2022-12-17 20:08:36 +0600

    finish exercise 4

---
+++ 04AllEvenNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите любое целое число: ");
+string input = Console.ReadLine() ?? "";
+
+int n = int.Parse(input);
+
+int count = 2;
+Console.Write($"{n} -> ");
+
+while (count < n)
+{
+    Console.Write($"{count}, ");
+    count += 2;
+}
+Console.Write($"{count}");

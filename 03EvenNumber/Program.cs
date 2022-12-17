e1d8eb1f313f60b7f53e7bd1e963664526936b26
Oleg <olegashepelev@gmail.com>
2022-12-17 18:20:30 +0600

    finish exercise 3

---
+++ 03EvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите любое целое число: ");
+string input = Console.ReadLine() ?? "";
+
+int number = int.Parse(input);
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine($"{input} -> yes");
+}
+else
+{
+    Console.WriteLine($"{input} -> no");
+}

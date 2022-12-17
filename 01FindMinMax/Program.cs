acea070597de16af195aa23f6c0845a838ab26a8
Oleg <olegashepelev@gmail.com>
2022-12-17 17:49:18 +0600

    finish exercisee 1

---
+++ 01FindMinMax/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите любое целое число: ");
+string input1 = Console.ReadLine() ?? "";
+
+Console.Write("Введите любое целое число: ");
+string input2 = Console.ReadLine() ?? "";
+
+int a = int.Parse(input1);
+int b = int.Parse(input2);
+
+if (a > b)
+{
+    Console.WriteLine($"a = {a}, b = {b} -> max = {a}");
+}
+else
+{
+    Console.WriteLine($"a = {a}, b = {b} -> max = {b}");
+}
